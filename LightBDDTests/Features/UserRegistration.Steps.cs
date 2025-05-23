7947a30696383815a5d401f61966c1b364fabd22
ivisca1 <ivisca1@etf.unsa.ba>
2025-05-23 15:02:37 +0200

    Implemented tests for registration, will split registration and login features into two different feature files with steps. This was done for clean code purposes.

---
+++ LightBDDTests/Features/UserRegistration.Steps.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebApp.Api;
+
+namespace LightBDDTests.Features;
+
+public partial class UserRegistration
+{
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+    private HttpResponseMessage _response;
+    private readonly string _email = "test@example.com";
+    private readonly string _password = "123456";
+
+    private Task Given_the_user_is_not_registered()
+    {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient();
+        return Task.CompletedTask;
+    }
+
+    private async Task When_the_user_submits_a_valid_email_and_password()
+    {
+        var request = new RegisterRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        _response = await _client.PostAsJsonAsync("/api/auth/register", request);
+    }
+
+    private Task Then_the_system_should_create_a_new_user_and_return_a_success_response()
+    {
+        _response.StatusCode.Should().Be(HttpStatusCode.OK);
+        return Task.CompletedTask;
+    }
+
+    private async Task Given_the_user_is_already_registered()
+    {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient();
+
+        var request = new RegisterRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        var response = await _client.PostAsJsonAsync("/api/auth/register", request);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    private async Task When_the_user_submits_the_same_email_again()
+    {
+        var request = new RegisterRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        _response = await _client.PostAsJsonAsync("/api/auth/register", request);
+    }
+
+    private Task Then_the_system_should_return_a_bad_request_response()
+    {
+        _response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        return Task.CompletedTask;
+    }
+
+}
---
+++ LightBDDTests/Features/UserRegistration.cs
@@ -0,0 +1,42 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit2;
+using System.Threading.Tasks;
+
+namespace LightBDDTests.Features;
+
+[FeatureDescription(
+    @"In order to create an account
+    As a new user
+    I want to register with valid credentials")]
+public partial class UserRegistration : FeatureFixture
+{
+    /*
+    Feature: User Registration
+
+      Scenario: Successful registration
+        Given the user is not registered
+        When the user submits a valid email and password
+        Then the system should create a new user and return a success response
+    */
+    [Scenario]
+    public async Task Successful_user_registration()
+    {
+        await Runner.RunScenarioAsync(
+            Given_the_user_is_not_registered,
+            When_the_user_submits_a_valid_email_and_password,
+            Then_the_system_should_create_a_new_user_and_return_a_success_response
+        );
+    }
+
+    [Scenario]
+    public async Task Registration_with_existing_email_should_fail()
+    {
+        await Runner.RunScenarioAsync(
+            Given_the_user_is_already_registered,
+            When_the_user_submits_the_same_email_again,
+            Then_the_system_should_return_a_bad_request_response
+        );
+    }
+}
+
