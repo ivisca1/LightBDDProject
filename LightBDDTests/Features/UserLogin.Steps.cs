8225c16e407c3df470d2b56fee77f9e09526fa33
ivisca1 <ivisca1@etf.unsa.ba>
2025-05-30 12:53:29 +0200

    login feature tests

---
+++ LightBDDTests/Features/UserLogin.Steps.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WebApp.Api;
+
+namespace LightBDDTests.Features;
+
+public partial class UserLogin
+{
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+    private HttpResponseMessage _response;
+    private readonly string _email = "login_test@example.com";
+    private readonly string _password = "Password123";
+
+    private async Task Given_the_user_is_already_registered()
+    {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient();
+
+        var registerRequest = new RegisterRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        await _client.PostAsJsonAsync("/api/auth/register", registerRequest);
+    }
+
+    private async Task When_the_user_submits_valid_login_credentials()
+    {
+        var loginRequest = new LoginRequest
+        {
+            Email = _email,
+            Password = _password
+        };
+
+        _response = await _client.PostAsJsonAsync("/api/auth/login", loginRequest);
+    }
+
+    private Task Then_the_system_should_return_a_successful_login_response()
+    {
+        _response.StatusCode.Should().Be(HttpStatusCode.OK);
+        return Task.CompletedTask;
+    }
+
+    private async Task When_the_user_submits_incorrect_login_credentials()
+    {
+        var loginRequest = new LoginRequest
+        {
+            Email = _email,
+            Password = "WrongPassword"
+        };
+
+        _response = await _client.PostAsJsonAsync("/api/auth/login", loginRequest);
+    }
+
+    private Task Then_the_system_should_return_an_unauthorized_response()
+    {
+        _response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        return Task.CompletedTask;
+    }
+}
---
+++ LightBDDTests/Features/UserLogin.cs
@@ -0,0 +1,33 @@
+﻿using LightBDD.Framework;
+using LightBDD.Framework.Scenarios;
+using LightBDD.XUnit2;
+using System.Threading.Tasks;
+
+namespace LightBDDTests.Features;
+
+[FeatureDescription(
+    @"In order to access my account
+    As a registered user
+    I want to be able to log in with my email and password")]
+public partial class UserLogin : FeatureFixture
+{
+    [Scenario]
+    public async Task Successful_login()
+    {
+        await Runner.RunScenarioAsync(
+            Given_the_user_is_already_registered,
+            When_the_user_submits_valid_login_credentials,
+            Then_the_system_should_return_a_successful_login_response
+        );
+    }
+
+    [Scenario]
+    public async Task Failed_login_due_to_invalid_credentials()
+    {
+        await Runner.RunScenarioAsync(
+            Given_the_user_is_already_registered,
+            When_the_user_submits_incorrect_login_credentials,
+            Then_the_system_should_return_an_unauthorized_response
+        );
+    }
+}
